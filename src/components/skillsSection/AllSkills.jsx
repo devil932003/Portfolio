d0bee916d5b2cdbda847d18a0240a3850fa62e0b
devil932003 <142711349+devil932003@users.noreply.github.com>
2025-05-06 23:28:04 +0530

    new

--- src/components/skillsSection/AllSkills.jsx
+++ src/components/skillsSection/AllSkills.jsx
@@ -68,7 +68,7 @@ const skills = [
 const AllSkills = () => {
   return (
     <div>
-      <div className="flex items-center justify-center relative gap-2 max-w-[1200px] mx-auto">
+      <div className="flex items-center justify-center relative gap-2 max-w-[1400px] mx-auto">
         {skills.map((item, index) => {
           return (
             <motion.div
